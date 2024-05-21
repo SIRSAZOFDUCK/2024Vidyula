eec818d8ce61aa570c03c6e6c8e547057527cb58
Saran <sirsazofduck@hotmail.com>
2024-05-21 21:22:37 +0100

    Changes to WLS regression

--- master.R
+++ master.R
@@ -1,6 +1,32 @@
-# Packages
-library(dplyr)
-library(ggplot2)
+## Instructions -------
+
+# 1. Change AHAH variable in line 38 as required (second variable in "select" function)
+# 2. Change plot label names in line 117
+# 3. Change QOF variable in lines 93, 102 and 115 as required
+# 4. Run code (note outputs not currently saved)
+
+
+## Add packages -------
+
+# Function to install packages if needed
+
+install_and_load_if_needed <- function(packages) {
+  # Find out which packages are not installed
+  not_installed <- packages[!(packages %in% installed.packages()[, "Package"])]
+  
+  # Install the missing packages
+  if(length(not_installed)) install.packages(not_installed)
+  
+  # Load the packages
+  sapply(packages, require, character.only = TRUE)
+}
+
+# Install / load packages
+
+required_packages <- c("ggplot2", "dplyr", "tidyr")
+install_and_load_if_needed(required_packages)
+
+## Data processing -------
 
 # Read in data
 
@@ -60,14 +86,35 @@ data.qof <- read.csv("relevant qofs.csv") %>%
   inner_join(data_wa, by = c("Practice.code" = "practice_code")) %>% # Add score data to QOF data, only where values exist
   filter(n.patients > 500)
 
+
+## Weighted least squares regression -------
+
+# Fit standard regression
+model <- lm(score ~ Prevalence.of.Obesity....2022.2023.in.age..18., data = data.qof)
+
+# Calculate residuals
+residuals <- resid(model)
+
+# Estimate weights as the inverse of the squared residuals 
+weights <- 1 / (residuals^2)
+
+# Perform the weighted least squares regression using the calculated weight
+wls.model <- lm(score ~ Prevalence.of.Obesity....2022.2023.in.age..18., data = data.qof, weights = weights)
+
+# Display the summary of the WLS model
+summary(wls.model)
+
+
+## Plot -------
+
+data.qof.2 <- data.qof # Make replicate of dataframe
+
+data.qof.2$fitted <- fitted(wls.model) # Add fitted values to the data frame
+
 # Plot
-ggplot(data.qof, aes(x = score, y = Prevalence.OF..non.diabetic.hyperglycaemia..2022.2023..in.age..18..)) +
-  geom_point() +  # Add points
-  labs(title = "Scatter Plot", x = "X LABEL", y = "Y LABEL") +  # Add labels
+ggplot(data.qof.2, aes(x = score, y = Prevalence.of.Obesity....2022.2023.in.age..18.)) +
+  geom_point(size = 0.3, colour = "#0D0B7E") +  # Add points
+  labs(title = "Scatter Plot Title", x = "X LABEL", y = "Y LABEL") +  # Add labels
+  #geom_smooth(method = "lm", se = FALSE, color = "#9B0404") + 
+  #geom_line(aes(y = fitted), color = "#9B0404") + # Add line of best fit
   theme_minimal() 
-
-# Regression
-
-model <- lm(score ~ Prevalence.OF..non.diabetic.hyperglycaemia..2022.2023..in.age..18.., data = data.qof)
-
-summary(model)
